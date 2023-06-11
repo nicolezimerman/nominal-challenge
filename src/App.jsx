8811ac8f762d7a45467f269d398d546c91204fbd
nzimerman <nicole.zimerman@gmail.com>
2023-06-11 19:27:56 +0300

    Base project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Nominal Challenge</h1>
+      <div className="main">Chart of accounts</div>
+    </>
+  );
+}
+
+export default App;
